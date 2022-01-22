3c1ba13f4d5631f2080b02cb45790f3265a21d95
tarwat-uddin <t.uddin9121@gmail.com>
2022-01-22 17:19:33 +0000

    Refractor index.js
    Major improvements

--- index.js
+++ index.js
@@ -1,62 +1,32 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
-const hostname = "localhost";
-const port = 8080;
-const dirname = "./";
 
-const server = http.createServer((req, res) => {
-  // Checking if requested file is a html page
-  switch (req.url) {
-    case "/":
-      res.setHeader("Content-Type", "text/html");
-      pathname = dirname.concat("index.html");
-      res.statusCode = 200;
-      break;
-    case "/about":
-      res.setHeader("Content-Type", "text/html");
-      pathname = dirname.concat("about.html");
-      res.statusCode = 200;
-      break;
-    case "/contact-me":
-      res.setHeader("Content-Type", "text/html");
-      pathname = dirname.concat("contact-me.html");
-      res.statusCode = 200;
-      break;
-    default:
-      res.setHeader("Content-Type", "text/html");
-      pathname = dirname.concat("404.html");
-      res.statusCode = 404;
-      break;
-  }
-  // Checking if requested file is a css page
-  if (req.url.indexOf(".css") !== -1) {
-    res.setHeader("Content-Type", "text/css");
-    res.statusCode = 200;
+const app = express();
+// Config variables
+const HOSTNAME = "localhost";
+const PORT = process.env.PORT || 8080;
+// Load static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-    switch (req.url) {
-      case "/style.css":
-        pathname = dirname.concat("style.css");
-        break;
-      case "/reset.css":
-        pathname = dirname.concat("reset.css");
-        break;
-    }
-  }
+app.get("/:name", (req, res) => {
+  const fileName = req.params.name + ".html";
+  const pathname = path.join(__dirname, "public", fileName);
 
-  fs.readFile(pathname, (err, data) => {
+  fs.readFile(pathname, (err) => {
     if (err) {
       console.error(err);
-      res.end();
+      res.sendFile(path.join(__dirname, "public", "404.html"), 404);
     } else {
-      res.end(data);
+      res.sendFile(pathname, 200);
     }
   });
 });
 
-server.listen(port, hostname, (err) => {
+app.listen(PORT, HOSTNAME, (err) => {
   if (err) {
     console.error(`Something went wrong ${err}`);
   } else {
-    console.log(`Server is listening on: ${port}`);
+    console.log(`Server is listening on: ${PORT}`);
   }
 });
